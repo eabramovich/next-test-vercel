f63783b82a8a2fba2e9ea2c6a6cef31d82a856ab
Евгения Абрамович <ewg.abramovich2014@yandex.ru>
2024-11-06 13:09:09 +0300

    Add catalog to home page

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const carBrands = [
+  {id: 1, name: 'Chery'},
+  {id: 2, name: 'Haval'},
+  {id: 3, name: 'Geely'},
+  {id: 4, name: 'Exeed'},
+  {id: 5, name: 'Omoda'},
+  {id: 6, name: 'Changan'},
+  {id: 7, name: 'Jaecoo'}
+]
